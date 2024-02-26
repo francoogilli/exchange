6a773c68c95b5fa9fdc7b5cc055a3798ff6a8e99
Franco Gilli <francogilli10@gmail.com>
2024-02-25 22:21:29 -0300

    Add prototype styles to cards.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <>
     <h1 className="text-3xl font-bold text-center text-blue-500">Hello</h1>
-    <div className="lg:grid text-white grid-cols-2 gap-3 p-2 lg:grid-cols-12 md:grid-cols-3 pb-14">
+    <div className="lg:grid text-white grid-cols-2 gap-3 space-y-2 sm:space-y-0 p-2 lg:grid-cols-12 md:grid-cols-3 pb-14">
     
-    <div className="border-2 col-span-5 p-6 bg-[#151823] border-zinc-800 rounded-2xl ">
+    <div className="border-2 col-span-5 p-6  bg-[#151823] border-zinc-800 rounded-2xl ">
       <div className="flex pb-2 justify-between">
         <h2 className="text-2xl text-zinc-200 font-normal">Exchange</h2>
         <Icon icon="uil:exchange-alt" className="text-green-400 text-3xl"/>
@@ -15,16 +15,23 @@ export default function Home() {
       <div className="flex pb-6 justify-between">
         <p className="text-2xl font-medium">1 <span className="text-teal-400">BTC</span></p>
         <Icon icon="pajamas:arrow-right" className="text-[#A6A9B3] text-3xl"/>
-        <p className="text-2xl font-medium">51,740 <span className="text-teal-400">ARS</span></p>
+        <p className="text-2xl font-medium">51,740 <span className="text-teal-400">USD</span></p>
       </div>
 
       <div className="pb-2">
-        <p className="text-xl text-teal-400 font-medium">Get</p>
-        <input type="number" name="" id="" placeholder="0" className="bg-[#151823] border border-zinc-800 rounded-2xl px-4 py-2 w-full" />
+        <p className="text-xl text-teal-400 font-medium">Obtienes</p>
+        <div className="flex justify-between p-2 items-center bg-[#151823] border border-zinc-800 rounded-2xl   ">
+          <input type="number" name="" id="" placeholder="0" className="appearance-none focus:outline-none bg-[#151823] text-lg w-3/4" />
+          <select name="" id="" className="focus:outline-none p-2 rounded-[0.5rem] bg-[#1a1d29]  text-base w-1/4">
+            <option value="">ARS</option>
+            <option value="">USD</option>
+            <option value="">BTC</option>
+          </select>
+        </div>
       </div>
 
       <div className="pb-5">
-        <p className="text-xl text-teal-400 font-medium">Pay</p>
+        <p className="text-xl text-teal-400 font-medium">Pagas</p>
         <div className="flex justify-between p-2 items-center bg-[#151823] border border-zinc-800 rounded-2xl   ">
           <input type="number" name="" id="" placeholder="0" className="appearance-none focus:outline-none bg-[#151823] text-lg w-3/4" />
           <select name="" id="" className="focus:outline-none p-2 rounded-[0.5rem] bg-[#1a1d29]  text-base w-1/4">
@@ -42,17 +49,17 @@ export default function Home() {
     
     
     
-    <div className="border col-span-7 border-zinc-800 rounded-2xl ">
-      <a href="/history" className="text-3xl text-center p-10">Historial</a>
+    <div className="border-2 bg-[#151823] flex justify-center items-center col-span-7 border-zinc-800 rounded-2xl ">
+      <a href="/history" className="text-3xl  font-medium text-center p-10">Historial</a>
     </div>
     
-    <div className="border col-span-4 border-zinc-800 rounded-2xl ">
-      <h2 className="text-3xl text-center p-10">Composicion</h2>
+    <div className="border-2 bg-[#151823] col-span-4 border-zinc-800 rounded-2xl ">
+      <h2 className="text-3xl font-medium text-center p-10">Composicion</h2>
     </div>
-    <div className="border col-span-4 border-zinc-800 rounded-2xl ">
+    <div className="border-2 font-medium bg-[#151823] col-span-4 border-zinc-800 rounded-2xl ">
     <h2 className="text-3xl text-center p-10">Estado Actual</h2>
     </div>
-    <div className="border col-span-4 border-zinc-800 rounded-2xl ">
+    <div className="border-2 font-medium bg-[#151823] col-span-4 border-zinc-800 rounded-2xl ">
       <h2 className="text-3xl text-center p-10">Ver precios</h2>
     </div>
 
