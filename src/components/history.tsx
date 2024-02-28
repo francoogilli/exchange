848b35e92bb1b023c212e5c1e1cad6e69737ef8f
Franco Gilli <francogilli10@gmail.com>
2024-02-27 21:03:50 -0300

    Increase font weight of tables

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -36,16 +36,16 @@ export default function History() {
                   index % 2 === 0 ? "bg-[#121212]" : "bg-[#121212]"
                 } dark:border-gray-700`}
               >
-                <td className="px-6 py-2 flex items-center ">
+                <td className="px-6 py-2 font-medium flex items-center ">
                   <Image src={item.icon} className="mr-3 hover:scale-125 transition-transform duration-500" width={25} height={25} alt=""/>
-                  <span className="font-normal">{item.currency}</span>
+                  <span className="font-medium">{item.currency}</span>
                 </td>
-                <td className="px-6 py-4">{item.price}</td>
-                <td className="px-6 py-4">{item.amount}</td>
-                <td className={`px-6 py-5 font-normal`}>
-                  <span className={`px-4 py-1 rounded-lg font-normal ${item.action === 'Vender' ? 'bg-emerald-500/20 hover:bg-emerald-500/25 hover:text-emerald-400 text-emerald-500 transition duration-300' : 'bg-red-500/20 hover:bg-red-500/25 hover:text-red-400 text-red-500 transition duration-300'}`}>{item.action}</span>
+                <td className="px-6 py-4 font-medium">{item.price}</td>
+                <td className="px-6 py-4 font-medium">{item.amount}</td>
+                <td className={`px-6 py-5 font-medium`}>
+                  <span className={`px-4 py-1 rounded-lg font-medium ${item.action === 'Vender' ? 'bg-emerald-500/20 hover:bg-emerald-500/25 hover:text-emerald-400 text-emerald-500 transition duration-300' : 'bg-red-500/20 hover:bg-red-500/25 hover:text-red-400 text-red-500 transition duration-300'}`}>{item.action}</span>
                 </td>
-                <td className="px-6 py-4">{item.date}</td>
+                <td className="px-6 py-4 font-medium">{item.date}</td>
               </tr>
             ))}
           </tbody>
