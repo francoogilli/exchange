f9f38b15ac9a5b31cc83e53d444acf098471f395
Franco Gilli <francogilli10@gmail.com>
2024-02-28 10:09:42 -0300

    Add draggable in false and select none in Balance component

--- src/components/balance.tsx
+++ src/components/balance.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function Balance() {
     return (
-        <div className="bg-[#121212] col-span-4 rounded-2xl p-5">
+        <div className="bg-[#121212] col-span-4 rounded-2xl select-none p-5">
             <h2 className="text-xl font-semibold text-center text-[#A9A9A9] pb-6">Tu balance</h2>
             <h2 className="text-5xl font-semibold text-center pb-4">304.9</h2>
             <div className="flex justify-center hover:scale-110 transition duration-500  items-center gap-x-2">
@@ -9,7 +9,7 @@ export default function Balance() {
                 <span className="font-semibold text-xl text-[#8C8C8C] ">ETH</span>
             </div>
             <div className="flex justify-center pb-7">
-                <Image src="/graphics.svg" className="w-full" width={310} height={310} alt="" />
+                <Image src="/graphics.svg" className="w-full" draggable="false" width={310} height={310} alt="" />
             </div>
         </div>
     );
