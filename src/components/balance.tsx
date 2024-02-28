5af844d36ae21ae3eaeb53ceb01e5d386257a39c
Franco Gilli <francogilli10@gmail.com>
2024-02-28 10:03:45 -0300

    Add hover scale to cripto in balance viewer

--- src/components/balance.tsx
+++ src/components/balance.tsx
@@ -4,9 +4,9 @@ export default function Balance() {
         <div className="bg-[#121212] col-span-4 rounded-2xl p-5">
             <h2 className="text-xl font-semibold text-center text-[#A9A9A9] pb-6">Tu balance</h2>
             <h2 className="text-5xl font-semibold text-center pb-4">304.9</h2>
-            <div className="flex justify-center items-center gap-x-2">
+            <div className="flex justify-center hover:scale-110 transition duration-500  items-center gap-x-2">
                 <Image src="/coins/eth.svg" width={25} height={25} alt="" />
-                <span className="font-semibold text-xl text-[#8C8C8C]">ETH</span>
+                <span className="font-semibold text-xl text-[#8C8C8C] ">ETH</span>
             </div>
             <div className="flex justify-center pb-7">
                 <Image src="/graphics.svg" className="w-full" width={310} height={310} alt="" />
