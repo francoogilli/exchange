0212b5eb67a73a2721f76fe4ca710d1c66ed413a
Franco Gilli <francogilli10@gmail.com>
2024-02-29 12:46:26 -0300

    Change color and font-weight Buy-Sell

--- src/components/buy-sell.tsx
+++ src/components/buy-sell.tsx
@@ -38,7 +38,7 @@ export default function BuySell() {
           </select>
         </div>
       </div>
-      <span className="text-zinc-500 text-base font-semibold">1 BTC = $62,955</span>
+      <span className="text-zinc-400 text-base font-medium">1 BTC = $62,955</span>
 
       <button className="font-medium text-xl px-3 py-4 mt-6 w-full bg-gradient-to-r from-purple-600 hover:from-purple-600 hover:to-purple-500 transition duration-300 to-purple-400 border border-zinc-800 rounded-2xl">
         Exchange
